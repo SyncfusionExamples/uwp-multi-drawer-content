625bbbe9f397353a2564721a7f7647b90364035f
Jeya Kasipandi <jeya.kasipandi@syncfusion.com>
2021-04-27 19:53:25 +0530

    Multi drawer content has been added

---
+++ NavigationDrawer/NavigationDrawer/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NavigationDrawer
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void button1_Clicked(object sender, EventArgs e)
+        {
+            navigationDrawer.ToggleDrawer();
+
+        }
+        private void button2_Clicked(object sender, EventArgs e)
+        {
+            navigationDrawer.ToggleSecondaryDrawer();
+        }
+    }
+}
